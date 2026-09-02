a72e6629fafb70048cc93e9760b4d4bdd387fb39
Manas-Dikshit <manasdikshit48@gmail.com>
2026-09-03 01:25:04 +0530

    chore: initialize correlation module

---
+++ signalscope_dsp/correlation/__init__.py
@@ -0,0 +1,9 @@
+from .correlate import (
+    CorrelationMatch, autocorrelate_bits, cross_correlate_bits,
+    sliding_pattern_match, find_repeated_sequences,
+)
+
+__all__ = [
+    "CorrelationMatch", "autocorrelate_bits", "cross_correlate_bits",
+    "sliding_pattern_match", "find_repeated_sequences",
+]
