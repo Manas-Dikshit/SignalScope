116e533bd1730472e1ae13cb5f6efc5c5291045a
Manas-Dikshit <manasdikshit48@gmail.com>
2026-09-15 00:56:05 +0530

    Add exports and run the full DSP suite.

--- services/dsp-worker/signalscope_dsp/fec/__init__.py
+++ services/dsp-worker/signalscope_dsp/fec/__init__.py
@@ -2,10 +2,12 @@ from .convolutional import convolutional_encode, viterbi_decode, ViterbiResult
 from .validation import bits_to_bytes, crc16_ccitt, validate_crc16, find_sync_word, count_crc_valid_frames
 from .reed_solomon import reed_solomon_encode, reed_solomon_decode, ReedSolomonResult
 from .ldpc import build_regular_ldpc, ldpc_encode, ldpc_decode, LDPCResult
+from .concatenated import concatenated_encode, concatenated_decode, ConcatenatedResult
 
 __all__ = [
     "convolutional_encode", "viterbi_decode", "ViterbiResult",
     "bits_to_bytes", "crc16_ccitt", "validate_crc16", "find_sync_word", "count_crc_valid_frames",
     "reed_solomon_encode", "reed_solomon_decode", "ReedSolomonResult",
     "build_regular_ldpc", "ldpc_encode", "ldpc_decode", "LDPCResult",
+    "concatenated_encode", "concatenated_decode", "ConcatenatedResult",
 ]
